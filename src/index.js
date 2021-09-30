3aaae25d6a67f3988f5da00552f54a27eb626d8b
Nguyễn Văn Điền <diennv@ttc-solutions.com.vn>
2021-10-01 00:01:37 +0700

    a

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ const express = require('express');
 const exphdb = require('express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
-const redis = require('redis')
-const session = require('express-session')
-
-let RedisStore = require('connect-redis')(session)
-let redisClient = redis.createClient()
-
+const session = require('express-session');
 const flash = require('connect-flash');
 const cookieParser = require('cookie-parser');
 require('dotenv').config()
@@ -20,30 +15,29 @@ app.set('views', path.join(__dirname, 'resources/views'))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(session({
-  store: new RedisStore({client: redisClient}),
-  saveUninitialized: false,
-  secret: 'keyboard cat',
-  resave: false,
-  cookie: {
-    maxAge: 60000,
-    secure: true,
-  }
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 60000,
+      secure: true,
+    }
 }));
 app.use(flash());
 app.use((req, res, next) => {
-  res.locals.message = req.session.message;
-  delete req.session.message;
-  next();
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
 });
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Connect MySQL
 const con = require("./config/db.js")
 
-app.use(function (req, res, next) {
-  req.con = con
-  next()
+app.use(function(req, res, next) {
+    req.con = con
+    next()
 })
 
 
@@ -81,6 +75,6 @@ app.use("/about-us", aboutUs);
 // const loginUser = require("./routesUser/login");
 // app.use("/", login);
 
-app.listen(process.env.PORT || process.env.APP_PORT, () => {
-  console.log('http://localhost:3001')
+app.listen(process.env.APP_PORT || process.env.PORT, () => {
+    console.log('http://localhost:3001')
 });
