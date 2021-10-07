ad719ec42df7e38bce1902eac04a4b7065a8806f
Nguyễn Văn Điền <diennv@ttc-solutions.com.vn>
2021-10-07 20:52:31 +0700

    s

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -152,7 +152,7 @@ class ProductController {
         if (err) return res.send(err)
         Product.DeleteImageProduct(req.con, req.params.id_product, (err, result) => {
           if (err) return res.send(err)
-          res.json(req.params.id_product)
+          return res.redirect('product')
         })
       })
     })
